3ca7900f4fee878b2b2b4e81a6bf630b73385169
ekong237 <liz.kong@yahoo.com>
2018-01-24 19:33:18 -0800

    flatten array

---
+++ flatten.js
@@ -0,0 +1,14 @@
+const flatten = (array) => {
+  return array.reduce((acc, el, i) => {
+    let add;
+    if (Array.isArray(el)) {
+      add = flatten(el)
+    } else {
+      add = el;
+    }
+    return acc.concat(add);
+  }, []);
+}
+
+let array = [1, [2, [3, 3, 3]], 4, [5, 6]];
+console.log(flatten(array));
