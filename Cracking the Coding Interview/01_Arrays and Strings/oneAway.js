9faaa5a3fce235dc779d6a5d0fc030b67eeed46d
ekong237 <liz.kong@yahoo.com>
2018-03-12 19:17:53 -0700

    one away from equal strings

--- Cracking the Coding Interview/01_Arrays and Strings/oneAway.js
+++ Cracking the Coding Interview/01_Arrays and Strings/oneAway.js
@@ -39,3 +39,36 @@ const oneAway = (str1, str2) => {
   return diff <= 1 && diff >= 0;
 }
 console.log(oneAway('pale', 'pale'));
+
+
+// Time: O(n)
+// Space: O(1)
+const oneAway2 = (str1, str2) => {
+  let longerStr = (str1.length > str2.length) ? str1 : str2;
+  let p1 = 0;
+  let p2 = 0;
+
+  if (Math.abs(str1, str2) > 1) {
+    return false;
+  } 
+
+  let editCount = 0;
+  while(p1 < longerStr.length && p2 < longerStr.length) {
+    if (str1[p1] !== str2[p2]) {
+      if (str1.length > str2.length) {
+        editCount++;
+        p1++;
+      } else {
+        editCount++;
+        p2++;
+      }
+    } else {
+      p1++;
+      p2++;
+    }
+  }
+  return editCount <= 1;
+}
+
+
+console.log(oneAway2('ple', 'pale'));
